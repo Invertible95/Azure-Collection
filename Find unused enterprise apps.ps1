d5de6c05df59047e3b7ac3c52f2dd34cead3524b
Victor Uhrberg <victor.uhrberg@advania.com>
2023-10-13 15:27:19 +0200

    Script finalize, Version 2.0

--- Find unused enterprise apps.ps1
+++ Find unused enterprise apps.ps1
@@ -4,27 +4,35 @@
 $tenantID = $env:TENANT_ID
 $clientID = $env:CLIENT_ID_EAPPS
 $clientSecretID = $env:CLIENT_SECRET_EAPPS
-
+$tokenResponse = $null  # Initialize to null
+$headers = @{}  # Initialize as an empty hashtable
 
 # Authenticate to MS Graph API and get headers
-$tokenBody = @{
-    Grant_Type    = "client_credentials"
-    Scope         = "https://graph.microsoft.com/.default"
-    Client_Id     = $clientID
-    Client_Secret = $clientSecretID
-}
-$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $tokenBody
-$headers = @{
-    "Authorization" = "Bearer $($tokenResponse.access_token)"
-    "Content-type"  = "application/json"
-}
+function Connect-toGraph {
 
+    $tokenBody = @{
+        Grant_Type    = "client_credentials"
+        Scope         = "https://graph.microsoft.com/.default"
+        Client_Id     = $clientID
+        Client_Secret = $clientSecretID
+    }
+    $tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $tokenBody
+    $global:headers = @{
+        "Authorization" = "Bearer $($tokenResponse.access_token)"
+        "Content-type"  = "application/json"
+    }
+    # Debug output
+    Write-Host "Access Token: $($tokenResponse.access_token)"
+    Write-Host "Headers: $($headers)"
+}
+Connect-toGraph
 # Function to retrieve information about Enterprise Apps from Microsoft Graph API
 function Get-EnterpriseApps {
     # Define the initial URL for fetching Enterprise Apps
     $URLGetApplications = "https://graph.microsoft.com/beta/servicePrincipals?" +
     "`$top=300&" + # Limit the number of results per page to 300
-    "`$filter=servicePrincipalType eq 'Application' and (tags/any(tag: tag eq 'WindowsAzureActiveDirectoryIntegratedApp'))"
+    "`$filter=servicePrincipalType eq 'Application' and (tags/any(tag: tag eq 'WindowsAzureActiveDirectoryIntegratedApp'))&" +
+    "`$select=appid,id,servicePrincipalNames"
 
     # Initialize an array to store the pages of Enterprise Apps
     $appPages = @()
@@ -47,7 +55,7 @@ function Get-EnterpriseApps {
     return $appPages
 }
 
-# Function to retrieve the count of sign-ins within a specified time frame
+# Function to retrieve the count of interactive sign-ins within a specified time frame
 function Get-SignInsInteractive { 
     param (
         [System.Collections.ArrayList] $Applications,
@@ -67,14 +75,15 @@ function Get-SignInsInteractive {
         $progressPercentage = [math]::Round(($currentIteration / $totalIterations) * 100)
         
         # Construct progress status
-        $progressMessage = "Fetching Sign-Ins for Application $($application.appDisplayName) ($currentIteration/$totalIterations) - App ID: $($application.appId)"
+        $progressMessage = "Fetching Interactive Sign-Ins for Application $($application.appDisplayName) ($currentIteration/$totalIterations) - App ID: $($application.appId)"
         Write-Progress -Status "Progress" -PercentComplete $progressPercentage -Activity $progressMessage
         
         # Construct the URL to fetch the count of sign-ins for the current application
         $URLSignInsForAppInteractive = "https://graph.microsoft.com/v1.0/auditLogs/signIns?" +
         "`$filter=appId eq '$($application.appId)' " + # Filter by the application's ID
         "and createdDateTime ge $($startDate.ToString('yyyy-MM-ddTHH:mm:ssZ')) " + # Start date filter
-        "and createdDateTime le $($endDate.ToString('yyyy-MM-ddTHH:mm:ssZ'))"  # End date filter
+        "and createdDateTime le $($endDate.ToString('yyyy-MM-ddTHH:mm:ssZ'))" +
+        " and id eq '$($application.Id)'"  # End date filter
 
         try {
             # Invoke a REST API GET request to fetch the count of sign-ins for the current application
@@ -84,6 +93,7 @@ function Get-SignInsInteractive {
             $signInCount[$application.appId] = @{
                 count       = $signIns.value.Count
                 DisplayName = $application.appDisplayName
+                ID = $application.Id
             }
         }
         catch {
@@ -122,7 +132,7 @@ function Get-SignInsNonInteractive {
         $progressPercentage = [math]::Round(($currentIteration / $totalIterations) * 100)
         
         # Construct progress status
-        $progressMessage = "Fetching Sign-Ins for Application $($application.appDisplayName) ($currentIteration/$totalIterations) - App ID: $($application.appId)"
+        $progressMessage = "Fetching Non-Interactive Sign-Ins for Application $($application.appDisplayName) ($currentIteration/$totalIterations) - App ID: $($application.appId)"
         Write-Progress -Status "Progress" -PercentComplete $progressPercentage -Activity $progressMessage
         
         # Construct the URL to fetch the count of sign-ins for the current application
@@ -130,9 +140,9 @@ function Get-SignInsNonInteractive {
         "&`$filter=(signInEventTypes/any(t: t eq 'nonInteractiveUser'))" +
         " and appId eq '$($application.appId)'" +
         " and createdDateTime ge $($startDate.ToString('yyyy-MM-ddTHH:mm:ssZ'))" +
-        " and createdDateTime le $($endDate.ToString('yyyy-MM-ddTHH:mm:ssZ'))"
+        " and createdDateTime le $($endDate.ToString('yyyy-MM-ddTHH:mm:ssZ'))" +
+        " and id eq '$($application.Id)'"
 
-   
         # Invoke a REST API GET request to fetch the count of sign-ins for the current application
         $signIns = Invoke-RestMethod -Method Get -Uri $URLSignInsForAppNonInteractive -Headers $headers
 
@@ -140,6 +150,7 @@ function Get-SignInsNonInteractive {
         $signInCount[$application.appId] = @{
             count       = $signIns.value.Count
             DisplayName = $application.appDisplayName
+            ID = $application.Id
         }
        
         # Introduce a delay to avoid rate limiting
@@ -150,31 +161,79 @@ function Get-SignInsNonInteractive {
     return $signInCount
 }
 
-
 # Set the time frame for sign-in retrieval (e.g., 1 day)
-$timeFrameInDays = 90
+$timeFrameInDays = 10
 
 # Retrieve Enterprise Apps and associated Sign-Ins
 $enterPriseApps = Get-EnterpriseApps
-$interActiveSignInsForApps = Get-SignInsInteractive -Applications $enterPriseApps -timeFrameInDays $timeFrameInDays
+$interActiveSignIns = Get-SignInsInteractive -Applications $enterPriseApps -timeFrameInDays $timeFrameInDays
+Connect-toGraph
 $nonInteractiveSignIns = Get-SignInsNonInteractive -Applications $enterPriseApps -timeFrameInDays $timeFrameInDays
 
+Connect-MgGraph
+# Function to remove inactive Enterprise Apps
+function Remove-InactiveEnterpriseApps {
 
-# Export the signInCounts hashtable to CSV
-$interActiveSignInsForApps = Get-SignInsInteractive -Applications $enterPriseApps -timeFrameInDays $timeFrameInDays
-$interactiveSignInsForApps.GetEnumerator() | ForEach-Object {
-    [PSCustomObject]@{
-        AppId       = $_.Key
-        Count       = $_.Value.Count
-        DisplayName = $_.Value.DisplayName
+    try {
+        $RemoveableApps = $interactiveSignIns.GetEnumerator() | Where-Object { ( $_.Value.Count -eq '0' ) } | `
+            ForEach-Object {
+            [PSCustomObject]@{
+                AppId       = $_.Key
+                Count       = $_.Value.Count
+                DisplayName = $_.Value.DisplayName
+                Id = $_.Value.Id
+            }
+        }
+        $RemoveApps = $RemoveableApps.Id
     }
-} | Export-Csv -Path "C:\temp\SignInCountsinterActive.csv" -NoTypeInformation -Encoding utf8
+    catch {
+        Write-Error -Verbose
+    }
 
-$nonInteractiveSignIns = Get-SignInsNonInteractive -Applications $enterpriseApps -timeFrameInDays $timeFrameInDays
-$nonInteractiveSignIns.GetEnumerator() | ForEach-Object {
-    [PSCustomObject]@{
-        AppId       = $_.Key
-        Count       = $_.Value.Count
-        DisplayName = $_.Value.DisplayName
+    foreach ($ServicePrincipalId in $RemoveApps) {
+        Remove-MgServicePrincipal -ServicePrincipalId $ServicePrincipalId
     }
-} | Export-Csv -Path "C:\temp\SignInCountsNonInteractive.csv" -NoTypeInformation -Encoding utf8
+
+    try {
+        $RemoveableApps2 = $nonInteractiveSignIns.GetEnumerator() | Where-Object { ( $_.Value.Count -eq '0' ) } | `
+            ForEach-Object {
+            [PSCustomObject]@{
+                AppId       = $_.Key
+                Count       = $_.Value.Count
+                DisplayName = $_.Value.DisplayName
+                Id = $_.Value.id
+            }
+        }
+        $RemoveApps2 = $RemoveableApps2.Id
+    }
+    catch {
+        Write-Error -Verbose
+    }
+
+    foreach ($ServicePrincipalId in $RemoveApps2) {
+        Remove-MgServicePrincipal -ServicePrincipalId $ServicePrincipalId
+    }
+}
+
+# Function to export sign-in counts to CSV files
+function Export-SignInsToCSV {
+    # Export the signInCounts hashtable to CSV for interactive sign-ins
+    $interactiveSignIns.GetEnumerator() | ForEach-Object {
+        [PSCustomObject]@{
+            AppId       = $_.Key
+            Count       = $_.Value.Count
+            DisplayName = $_.Value.DisplayName
+            Id = $_.Value.id
+        }
+    } | Export-Csv -Path "C:\temp\SignInCountsInteractive.csv" -NoTypeInformation -Encoding utf8
+
+    # Export the signInCounts hashtable to CSV for non-interactive sign-ins
+    $nonInteractiveSignIns.GetEnumerator() | ForEach-Object {
+        [PSCustomObject]@{
+            AppId       = $_.Key
+            Count       = $_.Value.Count
+            DisplayName = $_.Value.DisplayName
+            Id = $_.Value.id
+        }
+    } | Export-Csv -Path "C:\temp\SignInCountsNonInteractive.csv" -NoTypeInformation -Encoding utf8  
+}
