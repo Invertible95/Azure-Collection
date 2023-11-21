a08a73a266e16c04f8a91a248c0e22104bde9103
Victor Uhrberg <victor.uhrberg@advania.com>
2023-11-21 14:17:22 +0100

    Going to Prod, Version 2.0. Added synopsis and comments.

--- App registration scripts/App registration - Find Secrets expirationdate.ps1
+++ App registration scripts/App registration - Find Secrets expirationdate.ps1
@@ -1,9 +1,24 @@
-###In Development###
-# Install the required module if not already installed
-# Install-Module -Name Microsoft.Graph.Authentication -Force -AllowClobber
-# Install-Module -Name ImportExcel -Force -Scope CurrentUser
+<#
+.SYNOPSIS
+This PowerShell script performs the following tasks:
+    - Connects to Microsoft Graph to retrieve information about Azure AD applications and their associated secrets.
+    - Generates a report on secrets nearing expiry.
+    - Requires appropriate permissions (Cloud Application Administrator role) to access Azure.
+    - Checks for the presence of required modules (Microsoft.Graph.Authentication and ImportExcel) and installs them if necessary.
+    - Exports a report to an Excel file with a timestamp, including application details, secret IDs, and expiry dates.
 
-# Import the required modules, make sure you have them installed from previous steps.
+.MODULE INSTALLATION CHECK
+To ensure the necessary modules are available, the script dynamically checks and installs them:
+
+    $modulesToCheck = @("Microsoft.Graph.Authentication", "ImportExcel")
+
+    foreach ($module in $modulesToCheck) {
+        if (-not (Get-Module -Name $module -ListAvailable)) {
+            Install-Module -Name $module -Force -AllowClobber
+        }
+    }
+#>
+# Import the required modules
 Import-Module Microsoft.Graph.Authentication
 Import-Module ImportExcel
 
@@ -15,20 +30,19 @@ $allApplications = @()
 $nextLink = $null
 
 do {
+    # Retrieve paginated results of all applications
     $applicationsPage = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/applications\$($nextLink -replace '\?', '&')"
- 
+    
     $allApplications += $applicationsPage.Value
- 
     $nextLink = $applicationsPage.'@odata.nextLink'
 } while ($nextLink)
 
 $retArr = @()
 
 foreach ($Application in $allApplications) {
-    
-
     $SecretURI = "https://graph.microsoft.com/v1.0/applications/$($Application.id)/passwordCredentials"
     try {
+        # Retrieve secrets for each application
         $FindSecretDates = Invoke-MgGraphRequest -Method GET -Uri $SecretURI
     }
     catch {
@@ -37,13 +51,14 @@ foreach ($Application in $allApplications) {
     }
         
     foreach ($expireDate in $FindSecretDates.Value) {
-        
+        # Process each secret and check expiry
         $expiryDateTime = [datetime]$expireDate.endDateTime
         $expiryDate = $expiryDateTime.Date
         
-        if ($expireDate -ne $null) {
+        if ($expiryDate -ne $null) {
             $daysUntilExpiry = ($expiryDate - (Get-Date).Date).Days
             if ($daysUntilExpiry -gt 30) {
+                # Create a custom object for secrets nearing expiry
                 $myObject = [PSCustomObject]@{
                     ApplicationName = $($Application.displayName)
                     "Object/app ID" = $($Application.id)
@@ -55,5 +70,7 @@ foreach ($Application in $allApplications) {
         } 
     }
 }   
+
+# Export the collected data to an Excel file with a timestamp
 $excelDate = (Get-Date -Format "yyyy-MM-dd")
 $retArr | Export-Excel -Path "C:\temp\Report $excelDate.xlsx" -Append -Title "App registration secrets report" -TitleBold -TitleSize 14 -AutoSize -FreezeTopRow -MaxAutoSizeRows 12 
