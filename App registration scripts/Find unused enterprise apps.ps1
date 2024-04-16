4894ca7b3ffcdca74bb323760a9b851d0af6ea01
Victor Uhrberg <victor.uhrberg@advania.com>
2024-04-16 08:46:48 +0200

    [Upload] Uploaded new files and changed order

--- App registration scripts/Find unused enterprise apps.ps1
+++ App registration scripts/Find unused enterprise apps.ps1
@@ -1,4 +1,15 @@
 # Unused enterprise apps in Azure.
+# This script is used to find and remove unused and unnecessary Enterprise Apps from Microsoft Azure / Entra.
+# It uses an Enterprise app of it's own to do this for now, but I will change this in the future to futureproof the script.
+# Observe that in the current state it also removes the app you're using to remove others. I plan to implement a change to make it not do this.
+# I haven't had time to do this yet.
+
+# All commands and operations are written in functions, so you can call them one by one aswell.
+# The Remove-InactiveEnterPriseApps and the Export-SignInsToCsv functions are not executed automaticaly, you have to run them manualy.
+# This is a failsafe so you can in advance check which apps you're getting and what properties it recieves and push to the hash-table.
+# I recommend you first run the script once with $timeFrameInDays changed to 1, then run export-signinstocsv and double-check you find what you want.
+# Only run this script if you know what it does. Please feel free to contact me with any questions you might have.
+
 
 # Connection details
 $tenantID = $env:TENANT_ID
@@ -9,7 +20,6 @@ $headers = @{}  # Initialize as an empty hashtable
 
 # Authenticate to MS Graph API and get headers
 function Connect-toGraph {
-
     $tokenBody = @{
         Grant_Type    = "client_credentials"
         Scope         = "https://graph.microsoft.com/.default"
@@ -26,11 +36,12 @@ function Connect-toGraph {
     Write-Host "Headers: $($headers)"
 }
 Connect-toGraph
+
 # Function to retrieve information about Enterprise Apps from Microsoft Graph API
 function Get-EnterpriseApps {
     # Define the initial URL for fetching Enterprise Apps
     $URLGetApplications = "https://graph.microsoft.com/beta/servicePrincipals?" +
-    "`$top=300&" + # Limit the number of results per page to 300
+    "`$top=300&" +
     "`$filter=servicePrincipalType eq 'Application' and (tags/any(tag: tag eq 'WindowsAzureActiveDirectoryIntegratedApp'))&" +
     "`$select=appid,id,servicePrincipalNames"
 
@@ -93,7 +104,7 @@ function Get-SignInsInteractive {
             $signInCount[$application.appId] = @{
                 count       = $signIns.value.Count
                 DisplayName = $application.appDisplayName
-                ID = $application.Id
+                ID          = $application.Id
             }
         }
         catch {
@@ -150,7 +161,7 @@ function Get-SignInsNonInteractive {
         $signInCount[$application.appId] = @{
             count       = $signIns.value.Count
             DisplayName = $application.appDisplayName
-            ID = $application.Id
+            ID          = $application.Id
         }
        
         # Introduce a delay to avoid rate limiting
@@ -162,7 +173,7 @@ function Get-SignInsNonInteractive {
 }
 
 # Set the time frame for sign-in retrieval (e.g., 1 day)
-$timeFrameInDays = 1
+$timeFrameInDays = 10
 
 # Retrieve Enterprise Apps and associated Sign-Ins
 $enterPriseApps = Get-EnterpriseApps
@@ -170,50 +181,59 @@ $interActiveSignIns = Get-SignInsInteractive -Applications $enterPriseApps -time
 Connect-toGraph
 $nonInteractiveSignIns = Get-SignInsNonInteractive -Applications $enterPriseApps -timeFrameInDays $timeFrameInDays
 
-Connect-MgGraph
+Connect-MgGraph  # It's unclear what this function does. Consider adding a comment explaining its purpose.
+
 # Function to remove inactive Enterprise Apps
 function Remove-InactiveEnterpriseApps {
-
     try {
+        # Get a list of interactive apps with no sign-ins
         $RemoveableApps = $interactiveSignIns.GetEnumerator() | Where-Object { ( $_.Value.Count -eq '0' ) } | `
             ForEach-Object {
             [PSCustomObject]@{
                 AppId       = $_.Key
                 Count       = $_.Value.Count
                 DisplayName = $_.Value.DisplayName
-                Id = $_.Value.Id
+                Id          = $_.Value.Id
             }
         }
         $RemoveApps = $RemoveableApps.Id
     }
     catch {
+        # Handle any errors or exceptions
         Write-Error -Verbose
     }
 
+    # Iterate through the list of apps to remove
     foreach ($ServicePrincipalId in $RemoveApps) {
+        # Remove the Service Principal associated with the app
         Remove-MgServicePrincipal -ServicePrincipalId $ServicePrincipalId
     }
 
     try {
+        # Get a list of non-interactive apps with no sign-ins
         $RemoveableApps2 = $nonInteractiveSignIns.GetEnumerator() | Where-Object { ( $_.Value.Count -eq '0' ) } | `
             ForEach-Object {
             [PSCustomObject]@{
                 AppId       = $_.Key
                 Count       = $_.Value.Count
                 DisplayName = $_.Value.DisplayName
-                Id = $_.Value.id
+                Id          = $_.Value.id
             }
         }
         $RemoveApps2 = $RemoveableApps2.Id
     }
     catch {
+        # Handle any errors or exceptions
         Write-Error -Verbose
     }
 
+    # Iterate through the list of non-interactive apps to remove
     foreach ($ServicePrincipalId in $RemoveApps2) {
+        # Remove the Service Principal associated with the app
         Remove-MgServicePrincipal -ServicePrincipalId $ServicePrincipalId
     }
 }
+
 
 # Function to export sign-in counts to CSV files
 function Export-SignInsToCSV {
@@ -223,7 +243,7 @@ function Export-SignInsToCSV {
             AppId       = $_.Key
             Count       = $_.Value.Count
             DisplayName = $_.Value.DisplayName
-            Id = $_.Value.id
+            Id          = $_.Value.id
         }
     } | Export-Csv -Path "C:\temp\SignInCountsInteractive.csv" -NoTypeInformation -Encoding utf8
 
@@ -233,7 +253,7 @@ function Export-SignInsToCSV {
             AppId       = $_.Key
             Count       = $_.Value.Count
             DisplayName = $_.Value.DisplayName
-            Id = $_.Value.id
+            Id          = $_.Value.id
         }
     } | Export-Csv -Path "C:\temp\SignInCountsNonInteractive.csv" -NoTypeInformation -Encoding utf8  
 }
